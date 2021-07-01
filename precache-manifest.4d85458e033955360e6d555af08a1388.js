66dd8b08e7ff1e10b48c9efdf73d1e51a18f87dd
Arpit Singh <arpit.singh@go-mmt.com>
2021-07-01 17:31:56 +0530

    Updates

---
+++ precache-manifest.4d85458e033955360e6d555af08a1388.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8183a32653da53752f5d89bba6647c",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "a1dcdb4bda5f8c3e64c1",
+    "url": "/Keeper-App/static/js/2.06b4c030.chunk.js"
+  },
+  {
+    "revision": "1248692d5d4ad56c8ad1",
+    "url": "/Keeper-App/static/js/main.77f62d3c.chunk.js"
+  },
+  {
+    "revision": "8c85bae05d866358124a",
+    "url": "/Keeper-App/static/js/runtime-main.fca5ae5c.js"
+  }
+]);
